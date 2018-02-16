b6857701c29e7b396b9c13344c09c4b957f7c345
Daria <sam29dar@gmail.com>
2018-02-16 23:27:29 +0300

    first commit

---
+++ DataPreparation/DataPreparation.R
@@ -0,0 +1,84 @@
+#install.packages("ggplot2")
+#install.packages("dplyr")
+
+library(ggplot2)
+library(dplyr)
+
+# type here the path to the dataset
+directory = "c:/Users/sam29/Projects/R/DataAnalysis/DataPreparation/Population_wrong.csv"
+
+# reading csv file
+population <- read.csv(directory, header = F, sep = ";", col.names = c("Sex", "Year", "Amount"))
+
+# let's see what we have (first six tuples)
+head(population)
+
+# now change our dataframe the way that
+# we only have a year and a number of people in general (men and women)
+population_total <- aggregate(list(Total = population$Amount), 
+                              list(Year = population$Year),
+                              sum)
+
+# also look at our new dataframe
+head(population_total)
+
+# plot total population in dots
+ggplot(population_total, aes(x=population_total$Year, y=population_total$Total)) +
+  geom_point(shape=21, fill="blue", color="darkred", size=1) +
+  labs(title="Total population of Iceland from 1841 to 2017", x="Year", y="Total") +
+  scale_x_continuous(breaks=seq(1840, 2020, by = 20)) +
+  scale_y_continuous(breaks=seq(100000, 900000, by = 50000))
+
+
+
+# looking for outliers using
+# Chebyshev inequality (mean(x)+-k*s, where s - standard deviation) whith k = 3
+# (we can also consider "-" but it is redundantly)
+outliers <- population_total %>% filter(Total >
+                              mean(population_total$Total) + 3 * sd(population_total$Total))
+
+# the left data is
+left <- population_total %>% filter(Total <=
+                                      mean(population_total$Total) + 3 * sd(population_total$Total))
+
+# approximating outliers 
+for (i in 1:length(outliers$Year)){
+  outliers$Total[i] <- tail(left, 1)$Total + ((mean(population_total$Total) + 3 * sd(population_total$Total) -
+                         tail(left, 1)$Total)/length(outliers$Year) * i)
+}
+
+# approximated data
+new_population_total <- data.frame(rbind(left, outliers))
+
+# let's see what we've done
+ggplot(new_population_total, aes(x=new_population_total$Year, y=unlist(new_population_total$Total))) +
+  geom_point(shape=21, fill="blue", color="darkred", size=1) +
+  labs(title="Total population of Iceland from 1841 to 2017 (approximated)", x="Year", y="Total") +
+  scale_x_continuous(breaks=seq(1840, 2020, by = 20)) +
+  scale_y_continuous(breaks=seq(100000, 900000, by = 50000))
+
+# check that the median is the same before and after transformations
+median_prev = median(population_total$Total)
+median_prev
+median_new = median(unlist(new_population_total$Total))
+median_new
+
+# boxplot showing the outlier in female group
+ggplot(population, aes(x=population$Sex, y=population$Amount)) +
+  geom_boxplot() + 
+  labs(title = "Population of Iceland by sex from 1841 to 2017",x="Sex", y="Total") +
+  scale_y_continuous(breaks=seq(100000, 600000, by = 50000))
+
+# boxplot on total population before and after approximation the outlier respectively
+options(stringsAsFactors = F)
+n <- data.frame(rbind(cbind(population_total$Total, group = "g1(before)"), 
+           cbind(unlist(new_population_total$Total), group = "g2(after)")))
+colnames(n) <- (c("Total", "Group"))
+n$Group <- as.factor(n$Group)
+n$Total <- as.integer(n$Total)
+
+ggplot(n, aes(n$Group, n$Total)) +
+  geom_boxplot() +
+  labs(title = "Population of Iceland from 1841 to 2017",
+       x="Before & After the approximation", y="Total") +
+  scale_y_continuous(breaks=seq(100000, 900000, by = 50000))
