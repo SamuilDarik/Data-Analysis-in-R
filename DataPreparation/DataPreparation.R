2a824e8594c8b394e32dec1c8e5c8d705a979406
Daria <sam29dar@gmail.com>
2018-02-17 16:42:37 +0300

    DataPreparation code refactoring, add DataVisualization

--- DataPreparation/DataPreparation.R
+++ DataPreparation/DataPreparation.R
@@ -5,18 +5,18 @@ library(ggplot2)
 library(dplyr)
 
 # type the path to the dataset here
-directory = "c:/Users/sam29/Projects/R/DataAnalysis/DataPreparation/Population_wrong.csv"
+directory <-  "c:/Users/sam29/Projects/R/Data-Analysis-in-R/DataPreparation/Population_wrong.csv"
 
 # reading csv file
-population <- read.csv(directory, header = F, sep = ";", col.names = c("Sex", "Year", "Amount"))
+population <- read.csv(directory, header=F, col.names=c("Sex", "Year", "Amount"))
 
 # let's see what we have (first six tuples)
 head(population)
 
 # now change our dataframe the way that
 # we only have a year and a number of people in general (men and women)
-population_total <- aggregate(list(Total = population$Amount), 
-                              list(Year = population$Year),
+population_total <- aggregate(list(Total=population$Amount), 
+                              list(Year=population$Year),
                               sum)
 
 # also look at our new dataframe
@@ -26,8 +26,8 @@ head(population_total)
 ggplot(population_total, aes(x=population_total$Year, y=population_total$Total)) +
   geom_point(shape=21, fill="blue", color="darkred", size=1) +
   labs(title="Total population of Iceland from 1841 to 2017", x="Year", y="Total") +
-  scale_x_continuous(breaks=seq(1840, 2020, by = 20)) +
-  scale_y_continuous(breaks=seq(100000, 900000, by = 50000))
+  scale_x_continuous(breaks=seq(1840, 2020, by=20)) +
+  scale_y_continuous(breaks=seq(100000, 900000, by=50000))
 
 # looking for outliers using
 # Chebyshev inequality (mean(x)+-k*s contains at least 8/9 of data, where s - standard deviation) with k = 3
@@ -52,31 +52,31 @@ new_population_total <- data.frame(rbind(left, outliers))
 ggplot(new_population_total, aes(x=new_population_total$Year, y=unlist(new_population_total$Total))) +
   geom_point(shape=21, fill="blue", color="darkred", size=1) +
   labs(title="Total population of Iceland from 1841 to 2017 (approximated)", x="Year", y="Total") +
-  scale_x_continuous(breaks=seq(1840, 2020, by = 20)) +
-  scale_y_continuous(breaks=seq(100000, 900000, by = 50000))
+  scale_x_continuous(breaks=seq(1840, 2020, by=20)) +
+  scale_y_continuous(breaks=seq(100000, 900000, by=50000))
 
 # check that the median is the same before and after transformations
-median_prev = median(population_total$Total)
+median_prev <-  median(population_total$Total)
 median_prev
-median_new = median(unlist(new_population_total$Total))
+median_new <-  median(unlist(new_population_total$Total))
 median_new
 
 # boxplot showing the outlier in male group
 ggplot(population, aes(x=population$Sex, y=population$Amount)) +
   geom_boxplot() + 
   labs(title = "Population of Iceland by sex from 1841 to 2017",x="Sex", y="Total") +
-  scale_y_continuous(breaks=seq(100000, 600000, by = 50000))
+  scale_y_continuous(breaks=seq(100000, 600000, by=50000))
 
 # boxplot on total population before and after approximating the outlier respectively
-options(stringsAsFactors = F)
-n <- data.frame(rbind(cbind(population_total$Total, group = "g1(before)"), 
-           cbind(unlist(new_population_total$Total), group = "g2(after)")))
+options(stringsAsFactors=F)
+n <- data.frame(rbind(cbind(population_total$Total, group="g1(before)"), 
+           cbind(unlist(new_population_total$Total), group="g2(after)")))
 colnames(n) <- (c("Total", "Group"))
 n$Group <- as.factor(n$Group)
 n$Total <- as.integer(n$Total)
 
 ggplot(n, aes(n$Group, n$Total)) +
   geom_boxplot() +
-  labs(title = "Population of Iceland from 1841 to 2017",
+  labs(title="Population of Iceland from 1841 to 2017",
        x="Before & After the approximation", y="Total") +
   scale_y_continuous(breaks=seq(100000, 900000, by = 50000))
---
+++ DataVisualization/DataVisualization.R
@@ -0,0 +1,107 @@
+#install.packages("ggplot2")
+#install.packages("tidyr")
+#install.packages("grid")
+#install.packages("gridExtra")
+library(ggplot2)
+library(tidyr)
+library(grid)
+library(gridExtra)
+
+# pie chart
+
+# type the path to the dataset here
+directory <- "c:/Users/sam29/Projects/R/Data-Analysis-in-R/DataVisualization/Passengers from abroad on luxury liners.csv"
+passengers <- read.csv(directory, skip=3, col.names=c("Country", "Total"))
+
+# let's leave only first four tuples and consider others as a sum
+passengers <- rbind(head(passengers, 4), 
+                    data.frame(Country="Other Countries", Total=sum(tail(passengers$Total), 11)))
+
+# preparations for a plot
+values <- as.numeric(as.vector(passengers$Total))
+labels <- as.vector(passengers$Country)
+slices <- round(100*values/sum(values), 1)
+slices_labels <- paste(slices, "%", sep="")
+
+# plot a pie chart
+pie(slices,labels=slices_labels, col=rainbow(length(slices)), cex=0.5,
+      main = "Passengers from abroad on luxury liners at the Port of Reykjavik")
+legend(1, 1, labels, cex=0.7, fill=rainbow(length(slices)))
+
+# bar chart
+
+# type the path to the dataset here
+directory <- "c:/Users/sam29/Projects/R/Data-Analysis-in-R/DataVisualization/Use of social media by individuals.csv"
+using_networks <- read.csv(directory, skip=3, col.names=c("Country", "Total"))
+
+# plot a bar chart
+ggplot(using_networks, aes(x=using_networks$Country, y=using_networks$Total)) +
+  geom_col(fill="pink", color="darkred", size=0.05) +
+  labs(title="Use of social media by individuals (networks), 2013", x="Country", y="People(thousands)") +
+  scale_y_continuous(breaks=seq(0, 60, by=5)) +
+  theme(axis.text.x=element_text(angle=90, hjust=1))
+
+# scatter chart
+
+# type the path to the dataset here
+directory <- "c:/Users/sam29/Projects/R/Data-Analysis-in-R/DataVisualization/Graduated students by age.csv"
+education <- read.csv(directory, skip=2, col.names=c("Level","Age", "Total"))
+
+# plot a scatter chart
+ggplot(education, aes(x=education$Age, y=education$Total)) +
+  geom_point(shape=21, fill="pink", color="darkred", size=4) +
+  labs(title="Graduated students by age, level - ISCED 3, 2013", x="Age", y="People") +
+  scale_y_continuous(breaks=seq(200, 2500, by=100)) +
+  theme(axis.text=element_text(size=6))
+
+# multi scatter chart
+
+# type the path to the dataset here
+directory <- "c:/Users/sam29/Projects/R/Data-Analysis-in-R/DataVisualization/Graduated students by age_1.csv"
+education <- read.csv(directory, skip=2,
+                      col.names=c("Age","1995-1996","1996-1997","1997-1998","1998-1999","1999-2000","2000-2001",
+                                    "2001-2002","2002-2003","2003-2004","2004-2005","2005-2006","2006-2007","2007-2008","2008-2009","2009-2010",
+                                    "2010-2011","2011-2012","2012-2013"))
+
+# transform the dataset
+new_education <- gather(education, "Years", "Total", -1)
+
+# plot a multiscatter chart
+ggplot(new_education, aes(new_education$Age, new_education$Total, col = new_education$Years)) +
+  geom_point() +
+  labs(title="Graduated students by age, level - ISCED 3, 1995 - 2013", x="Age", y="People", color = "Year") +
+  scale_y_continuous(breaks=seq(200, 2500, by=100)) +
+  theme(axis.text=element_text(size=6), legend.text=element_text(size=6))
+
+# parallel coord
+
+# type the path to the dataset here
+directory <- "c:/Users/sam29/Projects/R/Data-Analysis-in-R/DataVisualization/Temperature in Reykjavik.csv"
+temperature <- read.csv(directory, col.names=c("Month", "Temperature"))
+temperature$Month <- factor(temperature$Month, levels=unique(temperature$Month))
+
+# type the path to the dataset here
+directory <- "c:/Users/sam29/Projects/R/Data-Analysis-in-R/DataVisualization/Hours of sunshine in Reykjavik.csv"
+hours_sunshine <- read.csv(directory, col.names = c("Month", "Hours"))
+hours_sunshine$Month <- factor(hours_sunshine$Month, levels=unique(hours_sunshine$Month))
+
+# bar chart for one dataset
+p <- ggplot(hours_sunshine, aes(x=hours_sunshine$Month,
+                                y=hours_sunshine$Hours)) +
+  geom_col(fill="pink", color="darkred", size=0.05) + 
+  labs(x="Month", y="Hours of sunshine") +
+  scale_y_continuous(breaks=seq(0, 190, by=10)) +
+  theme(axis.text=element_text(size=6))
+
+# dot plot for another
+q <- ggplot(temperature, aes(x=temperature$Month, y=temperature$Temperature, group=1)) +
+  geom_point(shape=21, fill="red", color="darkred", size=4) + geom_line(color="red") +
+  labs(y="Temperature") +
+  scale_y_continuous(breaks=seq(-1, 12, by=1)) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.text.y=element_text(size=6))
+
+# union these two plots
+grid.arrange(q, p, top="Temperature & Hours of sunshine in Reykjavik, 2014")
